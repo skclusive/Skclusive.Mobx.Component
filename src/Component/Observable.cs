32bb7df82f70a2c53041b9d383cf5efd5d6fd03e
senthilnathan sk <senthilnathansk@senthilnathans-MacBook-Pro.local>
2019-11-10 20:03:08 +0530

    Initial commit

---
+++ src/Component/Observable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Skclusive.Mobx.Component
+{
+    public class Observable : ObservableComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            ChildContent?.Invoke(builder);
+        }
+    }
+}
---
+++ src/Component/ObservableComponentBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Skclusive.Mobx.Observable;
+
+namespace Skclusive.Mobx.Component
+{
+    public class ObservableComponentBase : PureComponentBase, IDisposable
+    {
+        private Reaction _reaction;
+
+        protected RenderFragment _baseRenderFragment;
+
+        public ObservableComponentBase()
+        {
+            _reaction = new Reaction("Observable", (_1) =>
+            {
+                StateHasChanged();
+            },
+            (_1, _2) =>
+            {
+                throw new Exception("Observable component exception");
+            }
+            );
+
+            _baseRenderFragment = _renderFragment;
+
+            _renderFragment = builder =>
+            {
+                _reaction.Track(() =>
+                {
+                    return Actions.AllowStateChanges<object>(false, () =>
+                    {
+                        _baseRenderFragment(builder);
+
+                        return null;
+                    });
+                });
+            };
+        }
+
+        protected virtual void Dispose()
+        {
+        }
+
+        void IDisposable.Dispose()
+        {
+            _reaction?.Dispose();
+
+            _reaction = null;
+
+            Dispose();
+        }
+    }
+}
