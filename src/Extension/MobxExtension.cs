4b523baec06164b503e3ecba0a1678705625d16b
skclusive <skclusive@outlook.com>
2020-09-15 13:49:16 +0530

    preview2

---
+++ src/Extension/MobxExtension.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using Skclusive.Core.Component;
+
+namespace Skclusive.Mobx.Component
+{
+    public static class MobxExtension
+    {
+        public static void TryAddMobxServices(this IServiceCollection services, ICoreConfig coreConfig)
+        {
+            services.TryAddScopedEnumerable<IComponentConfigurer, MobxComponentConfigurer>();
+
+            services.TryAddCoreServices(coreConfig);
+        }
+    }
+}
